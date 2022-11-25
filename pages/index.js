f84c7cd0749d557d4892903cc4abbf76a9a406d9
Juan Cruz Cortiñas <juancruzcor@hotmail.com>
2022-11-25 12:27:31 -0300

    Definimos background color e implementamos botón para cambiar el color del mismo, funcionando como espejo

--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,32 @@
+import { useState } from 'react'
 import Image from 'next/image'
 import PageLayOut from '../components/layout/PageLayOut'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+
+const [colorTheme, setColorTheme] = useState(true)
+
+function changeBackgroundColor() {
+  colorTheme ? setColorTheme(false) : setColorTheme(true)
+}
+
   return (
     
     <PageLayOut title="Portfolio JC">
-      <div className={styles.container}>
-      
+      <div className={colorTheme ? styles.container : styles.container2}>
+
+        <div className={styles.divButton}>
+          <button 
+          onClick={changeBackgroundColor} 
+          className={colorTheme ? styles.principalColor : styles.secondaryColor}>
+            <Image src="/mode.png" alt='img change mode' width={40} height={40} className={styles.ColorTheme}/>
+          </button>
+        </div>
+
+
         <main className={styles.main}>
-          <h1 className={styles.title}>
-            Welcome to <a href="https://nextjs.org">Next.js!</a>
-          </h1>
-
-          <p className={styles.description}>
-            Get started by editing{' '}
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-
-          <div className={styles.grid}>
-            <a href="https://nextjs.org/docs" className={styles.card}>
-              <h2>Documentation &rarr;</h2>
-              <p>Find in-depth information about Next.js features and API.</p>
-            </a>
-
-            <a href="https://nextjs.org/learn" className={styles.card}>
-              <h2>Learn &rarr;</h2>
-              <p>Learn about Next.js in an interactive course with quizzes!</p>
-            </a>
-
-            <a
-              href="https://github.com/vercel/next.js/tree/canary/examples"
-              className={styles.card}
-            >
-              <h2>Examples &rarr;</h2>
-              <p>Discover and deploy boilerplate example Next.js projects.</p>
-            </a>
-
-            <a
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.card}
-            >
-              <h2>Deploy &rarr;</h2>
-              <p>
-                Instantly deploy your Next.js site to a public URL with Vercel.
-              </p>
-            </a>
-          </div>
+          
         </main>
 
         <footer className={styles.footer}>
