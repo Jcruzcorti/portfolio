4dd8fbdc8b3b29783a3e506105d398f0315d9d7a
Juan Cruz Cortiñas <juancruzcor@hotmail.com>
2022-12-01 10:48:11 -0300

    Comenzamos a utilizar Next UI para estilar las cards de los proyectos en componente Proyectos.jsx

---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { CssBaseline } from '@nextui-org/react';
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: React.Children.toArray([initialProps.styles])
+    };
+  }
+
+  render() {
+    return (
+      <Html lang="es">
+        <Head>{CssBaseline.flush()}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
