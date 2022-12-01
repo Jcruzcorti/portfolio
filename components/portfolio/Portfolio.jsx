8c78297e0ef412975d5d8c3db52f268f5d99f2e8
Juan Cruz Cortiñas <juancruzcor@hotmail.com>
2022-12-01 18:36:42 -0300

    Agregamos y linkeamos cada proyecto del portfolio con sus respectivas

--- components/portfolio/Portfolio.jsx
+++ components/portfolio/Portfolio.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import styles from '../../styles/Home.module.css'
 import Proyectos from '../proyectos/Proyectos'
+import img1 from '../../public/hp2.png'
+import img2 from '../../public/portfolio.png'
+import img3 from '../../public/scaloneta.png'
+import img4 from '../../public/nike.png'
+import img5 from '../../public/mmchocolates.png'
+import img6 from '../../public/mmchocolates.png'
+import img7 from '../../public/jh.png'
+import img8 from '../../public/cvviejo.png'
+import Link from 'next/link'
 
 
 function Portfolio() {
@@ -10,12 +19,18 @@ function Portfolio() {
     <div className={styles.divPortfolio} id={'Portfolio'}>
         <h2 className={styles.h2}>Proyectos</h2>
         <div className={styles.divProyectos}>
-          <Proyectos title='Harry Potter Quiz'/>
-          <Proyectos title='Harry Potter Quiz'/>
-          <Proyectos title='Harry Potter Quiz'/>
-          <Proyectos title='Harry Potter Quiz'/>
-          <Proyectos title='Harry Potter Quiz'/>
-            
+          <Link href={''} target='_blank'><Proyectos title='Portfolio' img={img2} priority={false} description='Desarrollo y maquetado de este mismo portfolio, con tecnologías como Next js Desarrollo y maquetado de este mismo portfolio, con tecnologías como Next js Desarrollo y maquetado de este mismo portfolio, con tecnologías como Next js'/></Link>            
+          <Link href={'https://harrypotterquizgame.vercel.app/'} target='_blank'><Proyectos title='Harry Potter Quiz' img={img1} description='Desarrollo y maquetado de este mismo portfolio, con tecnologías como Next js Desarrollo y maquetado de este mismo portfolio, con tecnologías como Next js Desarrollo y maquetado de este mismo portfolio, con tecnologías como Next js'/></Link>
+          <Link href={'https://scalonets.netlify.app/'} target='_blank'><Proyectos title='Scaloneta Quiz' img={img3} description='Desarrollo y maquetado de este mismo portfolio, con tecnologías como Next js Desarrollo y maquetado de este mismo portfolio, con tecnologías como Next js Desarrollo y maquetado de este mismo portfolio, con tecnologías como Next js'/></Link>
+        </div>
+        <div className={styles.divProyectos}>
+          <Link href={'https://mmchocolates.netlify.app/'} target='_blank'><Proyectos title='MM Chocolates app' img={img5} description='Desarrollo y maquetado de este mismo portfolio, con tecnologías como Next js Desarrollo y maquetado de este mismo portfolio, con tecnologías como Next js Desarrollo y maquetado de este mismo portfolio, con tecnologías como Next js'/></Link>
+          <Link href={'https://thenikestore.netlify.app/'} target='_blank'><Proyectos title='Nike Store' img={img4} description='Desarrollo y maquetado de este mismo portfolio, con tecnologías como Next js Desarrollo y maquetado de este mismo portfolio, con tecnologías como Next js Desarrollo y maquetado de este mismo portfolio, con tecnologías como Next js'/></Link>
+          <Link href={'https://jcruzcorti.github.io/MMchocolates/'} target='_blank'><Proyectos title='MM Chocolates sitio web' img={img6} description='Desarrollo y maquetado de este mismo portfolio, con tecnologías como Next js Desarrollo y maquetado de este mismo portfolio, con tecnologías como Next js Desarrollo y maquetado de este mismo portfolio, con tecnologías como Next js'/></Link>
+        </div>
+        <div className={styles.divProyectos}>
+          <Link href={'https://jcruzcorti.github.io/JH/'} target='_blank'><Proyectos title='Jugadores Históricos' img={img7} description='Desarrollo y maquetado de este mismo portfolio, con tecnologías como Next js Desarrollo y maquetado de este mismo portfolio, con tecnologías como Next js Desarrollo y maquetado de este mismo portfolio, con tecnologías como Next js'/></Link>
+          <Link href={'https://jcruzcorti.github.io/CV/'} target='_blank'><Proyectos title='Curriculum Vitae' img={img8} description='Desarrollo y maquetado de este mismo portfolio, con tecnologías como Next js Desarrollo y maquetado de este mismo portfolio, con tecnologías como Next js Desarrollo y maquetado de este mismo portfolio, con tecnologías como Next js'/> </Link>
         </div>
     </div>
   )
--- components/proyectos/Proyectos.jsx
+++ components/proyectos/Proyectos.jsx
@@ -3,21 +3,20 @@ import Image from 'next/image'
 import styles from '../../styles/Home.module.css'
 
 
-function Proyectos({title}) {
+function Proyectos({title,description,img}) {
 
 
   return (
     <div className={styles.divProyectos}>
        <div  className={styles.divCard1}>
-            <h4 className={styles.h4}> {title}</h4>
-            <div>
-                <p className={styles.pFooter}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat rem labore error. Sint, sunt ratione distinctio tempora nisi iste quidem autem recusandae iure quo culpa ab, neque voluptates? Ipsum, soluta.</p>
-                <p className={styles.pFooter}>Ver</p>
+            <h3 className={styles.h3Card}> {title}</h3>
+            <div className={styles.divImgCard}>
+                <Image src={img} alt='img change mode' layout='responsive' priority={true} width={100} height={100} className={styles.imgCard}/>
+            </div>
+            <div className={styles.divInfoCard}>
+                <p className={styles.pFooter}>{description}</p>
             </div>
        </div>
-       {/* <div  className={styles.divCard1}>
-            <h2 className={styles.h2}> {title}</h2>
-       </div> */}
     </div>
   )
 }
