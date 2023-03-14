ba2cdc2428522ea52ffd184c27efd3b09a4f863d
Juan Cruz Cortiñas <juancruzcor@hotmail.com>
2023-03-14 12:29:45 -0300

    We delete code that will not be used and commented out

--- components/portfolio/Portfolio.jsx
+++ components/portfolio/Portfolio.jsx
@@ -1,16 +1,6 @@
 import React from 'react'
 import styles from '../../styles/Home.module.css'
 import Proyectos from '../proyectos/Proyectos'
-import img1 from '../../public/hp2.png'
-import img2 from '../../public/newportfolio.png'
-import img3 from '../../public/scaloneta.png'
-import img4 from '../../public/nike.png'
-import img5 from '../../public/mmchocolates.png'
-import img6 from '../../public/mmchocolates.png'
-import img7 from '../../public/jh.png'
-import img8 from '../../public/cvviejo.png'
-import img9 from '../../public/raizweb3.png'
-import Link from 'next/link'
 import { Grid } from '@mui/material'
 import {proyects} from '../mock/Proyects'
 
@@ -28,23 +18,6 @@ function Portfolio() {
               <Proyectos key={proyect.id} item={proyect}/>
           ))}
         </Grid>
-
-        {/* <div className={styles.divProyectos}>
-          <Link href={'https://raizcarpinteria.vercel.app/'} target='_blank'><Proyectos title='Raiz Carpintería de Diseño' img={img9} description='Desarrollo de aplicación web para empresa Raiz Carpintería de Diseño, utilizando tencologías como Next.js y Material UI para confeccionar y estilar la aplicación '/></Link>
-          <Link href={'https://portfoliojcc.vercel.app/'} target='_blank'><Proyectos title='Portfolio' img={img2} priority={false} description='Desarrollo y re-versión de Curriculum, transformándo el sitio web anterior en una app más potente con tecnologías avanzadas como JavaScript, React.js y Next.js'/></Link>            
-          <Link href={'https://harrypotterquizgame.vercel.app/'} target='_blank'><Proyectos title='Harry Potter Quiz' img={img1} description='Quiz app con temática Harry Potter utilizando tencologías como JavaScript, React js, Next js y Firebase con el fin de guardar puntuación y nombre del participante  '/></Link>
-        </div>
-        <div className={styles.divProyectos}>
-        <Link href={'https://scalonets.netlify.app/'} target='_blank'><Proyectos title='Scaloneta Quiz' img={img3} description='Quiz app sobre Selección Argentina utilizando tencologías como JavaScript, React js y Firebase con la finalidad de almacenar nombre y puntuación del participante'/></Link>
-          <Link href={'https://mmchocolates.netlify.app/'} target='_blank'><Proyectos title='MM Chocolates app' img={img5} description='Creamos reversión de sitio web Mm Chocolates, transformándo el mismo en un e-commerce con tecnologías como JavaScript, React.js y Firebase almacenando la orden de compra hecha'/></Link>
-          <Link href={'https://thenikestore.netlify.app/'} target='_blank'><Proyectos title='Nike Store' img={img4} description='Desarrollo de e-commerce Nike app con tecnologías avanzadas como JavaScript, React.js, Adobe Photoshop y Firebase con el fin de guardar la orden de compra realizada'/></Link>
-          
-        </div>
-        <div className={styles.divProyectos}>
-          <Link href={'https://jcruzcorti.github.io/MMchocolates/'} target='_blank'><Proyectos title='MM Chocolates sitio web' img={img6} description='Desarrollo y maquetación de sitio web de emprendimiento de chocolates, con tecnologías básicas como HTML, CSS, Bootstrap y JavaScript consumiendo API Email.js para enviar pedido'/></Link>
-          <Link href={'https://jcruzcorti.github.io/JH/'} target='_blank'><Proyectos title='Jugadores Históricos' img={img7} description='Proyecto de Blog futbolero sobre una selección de jugadores de fútbol favoritos históricos, actuales y futuros, desarrollado y maquetado con tecnologías básicas como HTML, CSS'/></Link>
-          <Link href={'https://jcruzcorti.github.io/CV/'} target='_blank'><Proyectos title='Curriculum Vitae' img={img8} description='Proyecto similar al portfolio actual, sitio web sobre CV con información personal y logros, pero desarrollado y maquetado con tecnologías básicas como HTML, CSS y Bootstrap'/> </Link>
-        </div> */}
     </div>
   )
 }
--- components/proyectos/Proyectos.jsx
+++ components/proyectos/Proyectos.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import Image from "next/image";
-import styles from '../../styles/Home.module.css'
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import Link from 'next/link';
-// import ImgModal from './ImgModal';
 
 
 
@@ -14,17 +11,6 @@ function Proyectos({item}) {
 
 
   return (
-    // <div className={styles.divProyectos}>
-    //    <div  className={styles.divCard1}>
-    //         <h3 className={styles.h3Card}> {title}</h3>
-    //         <div className={styles.divImgCard}>
-    //             <Image src={img} alt='img proyectos' priority={true} sizes="100vw" width={310} height={180} className={styles.imgCard}/>
-    //         </div>
-    //         <div className={styles.divInfoCard}>
-    //             <p className={styles.pCard}>{description}</p>
-    //         </div>
-    //    </div>
-    // </div>
     <>
       <Link href={item.url} target='_blank'>
         <Card sx={{maxWidth: 400, my: 3, }} elevation={8}>    
@@ -42,8 +28,6 @@ function Proyectos({item}) {
               <Typography variant="body1" color="text.secondary">
                 {item.description}
               </Typography>
-
-            {/* <ImgModal img1={item.img} img2={item.img2} img3={item.img3}/> */}
           </CardContent>
         </Card>
       </Link>
