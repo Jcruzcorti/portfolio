9ceaa059940d0df5353a30944ba6d1d435403db3
Juan Cruz Cortiñas <juancruzcor@hotmail.com>
2024-02-07 11:12:08 -0300

    Added new project in portfolio

--- components/mock/Proyects.jsx
+++ components/mock/Proyects.jsx
@@ -3,6 +3,13 @@ export const proyects = [
 
     {
         id:'1',
+        tittle:'JC PERSONAL TRAINER',
+        description:'Desarrollo de landing page Jc personal trainer con el objetivo de difundir el trabajo de un grupo de entrenadores de Córdoba Capital, creada con Next.js 14, Typescript y diseñada con Tailwind y Shadcn',
+        url:'https://jcpersonaltrainer.vercel.app/',
+        img:'/jcptrainer.png'      
+    },
+    {
+        id:'2',
         tittle:'QUIEN JUEGA HOY',
         description:'Agenda deportiva de fútbol exclusivamente, con el fin de informar sobre los partidos de hoy y mañana, con horario argentino. Utilicé Next js y Material UI, consumiendo API de fútbol',
         url:'https://quienjuegahoy.vercel.app/',
@@ -10,7 +17,7 @@ export const proyects = [
     },
 
     {
-        id:'2',
+        id:'3',
         tittle:'RAIZ CARPINTERÍA DE DISEÑO',
         description:'Desarrollo de aplicación web para empresa Raiz Carpintería de Diseño, utilizando tencologías como Next.js y Material UI con estilos propios para confeccionar la aplicación',
         url:'https://raizcarpinteria.vercel.app/',
@@ -18,7 +25,7 @@ export const proyects = [
     },
 
     {
-        id:'3',
+        id:'4',
         tittle:'PORTFOLIO',
         description:'Desarrollo y re-versión de Curriculum, transformándo el sitio web anterior en una app más potente con tecnologías avanzadas como JavaScript, React.js y Next.js',
         url:'https://portfoliojcc.vercel.app/',
@@ -26,7 +33,7 @@ export const proyects = [
     },
     
     {
-        id:'4',
+        id:'5',
         tittle:'HARRY POTTER QUIZ',
         description:"Quiz app de 23 preguntas con temática Harry Potter utilizando tencologías como JavaScript, React js, Next js y Firebase con el fin de guardar puntuación y datos del participante",
         url:'https://harrypotterquizgame.vercel.app/',
@@ -34,7 +41,7 @@ export const proyects = [
     },
     
     {
-        id:'5',
+        id:'6',
         tittle:'SCALONETA QUIZ',
         description:"Quiz app sobre Selección Argentina utilizando tencologías como JavaScript, React js y Firebase con la finalidad de almacenar nombre y puntuación del participante",
         url:'https://scalonets.netlify.app/',
@@ -42,7 +49,7 @@ export const proyects = [
     },
     
     {
-        id:'6',
+        id:'7',
         tittle:'MM CHOCOLATES APP',
         description:'Creamos reversión de sitio web Mm Chocolates, transformándo el mismo en un e-commerce con tecnologías como JavaScript, React.js y Firebase almacenando la orden de compra hecha',
         url:'https://mmchocolates.netlify.app/',
@@ -50,7 +57,7 @@ export const proyects = [
     },
     
     {
-        id:'7',
+        id:'8',
         tittle:'NIKE STORE',
         description:'Desarrollo de e-commerce Nike app con tecnologías avanzadas como JavaScript, React.js, Adobe Photoshop y Firebase con el fin de guardar la orden de compra realizada por el usuario',
         url:'https://thenikestore.netlify.app/',
@@ -58,7 +65,7 @@ export const proyects = [
     },
     
     {
-        id:'8',
+        id:'9',
         tittle:'MM CHOCOLATES SITIO WEB',
         description:'Desarrollo y maquetación de sitio web de emprendimiento de chocolates, con tecnologías básicas como HTML, CSS, Bootstrap y JavaScript consumiendo API Email.js para enviar pedido',
         url:'https://jcruzcorti.github.io/MMchocolates/',
@@ -66,7 +73,7 @@ export const proyects = [
     },
     
     {
-        id:'9',
+        id:'10',
         tittle:'JUGADORES HISTÓRICOS',
         description:'Proyecto de Blog futbolero sobre una selección de jugadores de fútbol favoritos históricos, actuales y futuros, desarrollado y maquetado con tecnologías básicas como HTML, CSS',
         url:'https://jcruzcorti.github.io/JH/',
@@ -74,7 +81,7 @@ export const proyects = [
     },
     
     {
-        id:'10',
+        id:'11',
         tittle:'CURRICULUM VITAE',
         description:'Proyecto similar al portfolio actual, sitio web sobre CV con información personal y logros, pero desarrollado y maquetado con tecnologías básicas como HTML, CSS y Bootstrap',
         url:'https://jcruzcorti.github.io/CV/',
